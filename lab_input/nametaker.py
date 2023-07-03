6dbbe067ae0f8e1c90b65d3151f87e62436d0444
steven-yarbrough <steventheengineer7@gmail.com>
2023-07-03 22:50:54 +0000

    happy monday

---
+++ lab_input/nametaker.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Alta3 Research | steven-yarbrough
+print() - display data to std out"""
+
+# below is a function containing our code
+def main():
+
+    #pause the program and wait for the user input to be provided
+    name = input("what is your name?:")
+
+    #pause the program and wait for the user input to be provided
+    day = input("What day of the week is it?:")
+
+    #prints the respone to previous questions
+    print("Hello", name + "! Happy", day + "!")
+
+# this calls main
+main()
+
+    
