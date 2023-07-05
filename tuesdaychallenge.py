51e528567dd47500a1f7375cddb2701eac70bdae
steven-yarbrough <steventheengineer7@gmail.com>
2023-07-05 20:03:22 +0000

    mixed lsits

---
+++ tuesdaychallenge.py
@@ -0,0 +1,11 @@
+#!/usr/env/python3
+
+mylist= [1,2,3,4,5, "Python"]
+
+name= input("What is your name?\n>`")
+
+# This is what you should see when print runs-
+# Hi <name>! Welcometo Day 2 of Python Training!
+print("Hi " + name.upper() + "! Welcome to Day " + str ( mylist[1] + " of "  mylis[5] +  " Training!")
+
+
