d2ae06b9933f51851b79b0c39723f3cb2a8abbf5
steven-yarbrough <steventheengineer7@gmail.com>
2023-07-06 23:11:20 +0000

    working on if, else logic

--- iftest2/ipask.py
+++ iftest2/ipask.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-ipchk = input("Apply an IP address: ") # this line now prompts the user for input
+"""Alta3 Research | RZFeeser
+   Conditionals - strings test true"""
 
-# a provided string will test true
+ipchk = "192.168.0.1"
+
+# a string tests as True
 if ipchk:
-   print("Looks like the IP address was set: " + ipchk) # indented under if
-else:    # if data is NOT provided
-print("You did not provide input.") # indented under else
+   print("Looks like the IP address was set: " + ipchk)
