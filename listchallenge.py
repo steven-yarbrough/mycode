60077308d64037735ee57b715d9cec2cb5da1bea
steven-yarbrough <steventheengineer7@gmail.com>
2023-07-06 16:02:09 +0000

    About complex lists

---
+++ listchallenge.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+heroes= ["Spidermian", "Batman", "Black Panther", "Wonder Woman", "Storm"]
+
+# PART 1
+# Print out your favorite character from this list! The output would look something like:
+print(f'My favorite character is{heroes[1]}!')
+
+
+# PART 2
+# Ask the user to pick a number between 1 and 100.
+# Convert the input into an integer.
+answer = input('Pick a number between 1 and 100. >>')
+answer2 = int(answer)
+answer = int(answer)
+print(type(answer))
+
+
+
+nums= [1, -5, 56, 987, 0]
+
+# PART 3
+# check out https://docs.python.org/3/library/functions.html or go to Google
+# use a built-in function to find which integer in nums is the biggest.
+# then, print out that number!
+print(max(nums))
+
