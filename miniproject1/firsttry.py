59c9e7c1065fdfeeed9239f993239f637365ef2d
steven-yarbrough <steventheengineer7@gmail.com>
2023-07-07 03:30:39 +0000

    using if else and elif

--- miniproject1/firsttry.py
+++ miniproject1/firsttry.py
@@ -7,6 +7,6 @@ number = int(input("Enter a number between 1 and 15: "))
 if number < 1 or number > 15:
     print("Did you even read the question, fool?")
 elif number % 2 == 0:  
-    print(number, "is even.")
+    print(number, "is even, look at you go! My little crayon eater is all grown up!.")
 else:
-    print(number, "is odd.")
+    print(number, "is odd, thats ok, God Loves Ugly.")
