10d068bc4050252aa76d9a0e952e9abe9b75d260
steven-yarbrough <steventheengineer7@gmail.com>
2023-07-10 16:40:09 +0000

    farmchoice

--- forloop/farmchoice.py
+++ forloop/farmchoice.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
-#for  loop that prins the contents of the list,dictionary
+#ask the user to pick a farm. Return the plants/animals that are raised on that farm.
 
 farms = [{"name": "NE Farm", "agriculture": ["sheep", "cows", "pigs", "chickens", "llamas", "cats"]},
          {"name": "W Farm", "agriculture": ["pigs", "chickens", "llamas"]},
          {"name": "SE Farm", "agriculture": ["chickens", "carrots", "celery"]}]
+farm_choice = input("Choose a farm (NE Farm, W Farm, or SE Farm): ")
 for farm in farms:
-    if farm["name"] == "NE Farm":
+    if farm["name"] == farm_choice:
         print(farm["agriculture"])
